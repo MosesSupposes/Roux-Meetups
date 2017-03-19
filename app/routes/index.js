e46c3fa1748b5db4a2b8ab099959ed3238833e81
Moses Samuel <mosamuel98@gmail.com>
2017-03-19 10:28:24 -0700

    change folder structure

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     res.send(`
-        <h1>Welcome</h1>
-        <p>Roux Academy Meetups puts together artists from all walks of life</p>
+        <head>
+            <link rel="stylesheet" type="text/css" href="css/style.css">
+        </head>
+        <body>
+            <h1>Welcome to Roux Academy!</h1>
+            <img src="/images/misc/background.jpg" alt="background" style="height: 300px">
+            <p>Roux Academy Meetups puts together artists from all walks of life</p>
+            <script src="/reload/reload.js"></script>
+        </body>
     `);
 });
 
--- app/routes/speakers.js
+++ app/routes/speakers.js
@@ -8,13 +8,18 @@ router.get('/speakers', (req,res) => {
         info += `
         <li>
             <h2>${item.name}</h2>
+            <img src="/images/speakers/${item.shortname}_tn.jpg" alt="speaker">
             <p>${item.summary}</p>
         </li>
         `
     }); 
     res.send(`
+        <link rel="stylesheet" type="text/css" href="/css/style.css">
+        <h1>Welcome</h1>
         <h1>Roux Academy Meetups</h1>
         ${info}
+        <script src="/reload/reload.js"></script>
+
     `);
 });
 
@@ -22,9 +27,14 @@ router.get('/speakers/:speakerid', (req,res) => {
     let dataFile = req.app.get('appData');
     let speaker = dataFile.speakers[req.params.speakerid];
     res.send(`
+        <link rel="stylesheet" type="text/css" href="/css/style.css">
+        <h1>Welcome</h1>
         <h1>${speaker.title}</h1>
         <h2>with ${speaker.name}</h2>
+        <img src="/images/speakers/${speaker.shortname}_tn.jpg" alt="background" style="height: 300px">
         <p>${speaker.summary}</p>
+        <script src="/reload/reload.js"></script>
+
     `);
 });
 
