f40c2de27e64be2f91960fe9028d1f91169d4ac4
Moses Samuel <mosamuel98@gmail.com>
2017-03-19 10:27:11 -0700

    automate the workflow with nodemon and reload

--- app/app.js
+++ app/app.js
@@ -1,10 +1,12 @@
-const dataFile = require('./data/speakers.json');
 const express = require('express');
+const reload = require('reload');
+const dataFile = require('./data/speakers.json');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.set('appData', dataFile);
 
+app.use(express.static('app/public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/speakers'));
 
@@ -12,3 +14,4 @@ const server = app.listen(app.get('port'), () => {
     console.log(`Listening on port ${app.get('port')}`);
 })
 
+reload(server, app);
