de92131b22fa54cf4ac77265e6365cfac58db852
Moses Samuel <mosamuel98@gmail.com>
2017-03-20 06:50:25 -0700

    populate the site with more content

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index')
+    let data = req.app.get('appData');
+    let pagePhotos = [];
+
+    // Combine images of all speakers into a single array(pagePhots).
+    data.speakers.forEach(item => pagePhotos = pagePhotos.concat(item.artwork))
+
+    res.render('index', {
+        pageTitle: 'Home',
+        artwork: pagePhotos,
+        pageID: 'home'
+    });
 });
 
 module.exports = router;
