4c576f8e2ef3c0db19e15f3fa48ee4140ba9e3c8
Moses Samuel <mosamuel98@gmail.com>
2017-03-19 23:55:53 -0700

    implement ejs as the view

--- app/app.js
+++ app/app.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.set('appData', dataFile);
+app.set('view engine', 'ejs');
+app.set('views', 'app/views');
 
 app.use(express.static('app/public'));
 app.use(require('./routes/index'));
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,17 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send(`
-        <head>
-            <link rel="stylesheet" type="text/css" href="css/style.css">
-        </head>
-        <body>
-            <h1>Welcome to Roux Academy!</h1>
-            <img src="/images/misc/background.jpg" alt="background" style="height: 300px">
-            <p>Roux Academy Meetups puts together artists from all walks of life</p>
-            <script src="/reload/reload.js"></script>
-        </body>
-    `);
+    res.render('index')
 });
 
 module.exports = router;
