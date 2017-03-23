106f20c03ef7e091e695f6f5b2853c7e234f7bf2
Moses Samuel <mosamuel98@gmail.com>
2017-03-23 11:08:32 -0700

    handle post and delete requests

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const feedbackData = require('../data/feedback.json');
 
 router.get('/api', (req, res) => {
     res.json(feedbackData)
 });
 
-module.exports = router;
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+
+router.post('/api', (req, res) => {
+    feedbackData.unshift(req.body);
+    fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData), 'utf-8', err => {
+        if (err) { console.log(err) };
+    });
+    res.json(feedbackData);
+});
+
+router.delete('/api/:id', (req, res) => {
+    feedbackData.splice(req.params.id, 1);
+    fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData), 'utf-8', err => {
+        if (err) { console.log(err) };
+    });
+    res.json(feedbackData);
+});
+
+module.exports = router; 
